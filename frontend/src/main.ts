2f83d16601c99c0ca3e530c9d949757ac4fcd97e
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-07-20 10:36:45 -0700

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { BASE_URL } from './configurations/HttpConfiguration'
-import filters from './plugins/filters'; // Import the plugin
+
 const app = createApp(App)
 
 app.use(router)
@@ -29,5 +29,5 @@ app.config.globalProperties.$filters = {
         return BASE_URL + link;
     },
 }
-app.use(filters); // Use the plugin
+
 app.mount('#app')
